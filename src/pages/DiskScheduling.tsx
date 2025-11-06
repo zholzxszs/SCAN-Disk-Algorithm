2a3a929d6dd58e3878da4799878453bb561ad7c9
Judiel Legaspina <legaspinajudiel10@gmail.com>
2025-11-06 15:41:29 +0800

    change cursor

--- src/pages/DiskScheduling.tsx
+++ src/pages/DiskScheduling.tsx
@@ -71,7 +71,7 @@ const DiskScheduling: React.FC = () => {
             </div>
             <button
               onClick={() => setShowInfo(!showInfo)}
-              className="px-6 py-3 bg-green-600 hover:bg-green-700 text-white font-poppins font-semibold rounded-xl transition-colors duration-200 flex items-center gap-2"
+              className="px-6 py-3 bg-green-600 hover:bg-green-700 cursor-pointer text-white font-poppins font-semibold rounded-xl transition-colors duration-200 flex items-center gap-2"
             >
               <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
